ad65132adf6686facee7921b32b4529b9b83c44e
Andrewerr <toddot@protonmail.com>
2023-06-29 14:47:28 +0200

    Revert "[recovery] [interface]: Show build incremental version on separate line"
    This reverts commit fca5dd5fccb90dbb344a89945fbd49fe646d82ca.

--- recovery.cpp
+++ recovery.cpp
@@ -886,8 +886,9 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)"),
-    "Build " + android::base::GetProperty("ro.vendor.build.version.incremental", "<unknown>")
+    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)") +
+        " (" + android::base::GetProperty("ro.vendor.build.version.incremental", "<unknown>") +
+        ")",
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");
